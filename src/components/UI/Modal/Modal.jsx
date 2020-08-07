dd5bbf8b4f0e34d0f69d7cfc0410d362502f28ec
mubeen <mubeenarman19@gmail.com>
2020-08-08 02:13:36 +0900

    improve performance

--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { Component} from 'react';
 import Classes from './Modal.module.css';
 import Aux from '../../../hoc/Auxalary'
 import BackDrop from '../BackDrop/BackDrop'
 
-const modal = (props) => {
-    return(    
-        <Aux>
-            <BackDrop show={props.show} clicked={props.modalRemove}/>
-            <div className={Classes.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }}>
-                {props.children}
-            </div>   
-        </Aux>     
-    );
+
+class Modal extends Component{
+
+    shouldComponentUpdate( nextProp, nextState){
+        return nextProp.show !== this.props.show
+    }
+    render(){
+        return(    
+            <Aux>
+                <BackDrop show={this.props.show} clicked={this.props.modalRemove}/>
+                <div className={Classes.Modal}
+                style={{
+                    transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: this.props.show ? '1' : '0'
+                }}>
+                    {this.props.children}
+                </div>   
+            </Aux>     
+        );
+    }
+
 }
-export default modal;
+export default Modal;
